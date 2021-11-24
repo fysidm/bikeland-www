4cc402de7c65f6e287761bc6bba0ac54bf50fadc
GeorgeWu <fysidm@gmail.com>
2021-11-24 17:06:29 +0800

    Header prototype

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,30 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faBicycle,
+  faRoute,
+  faUmbrellaBeach,
+} from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
+
 const Header = () => {
+  const navigate = useNavigate();
   return (
     <header className="header-container">
       <img alt="bike-land-logo" src="logo.png" />
-      <div>Menu</div>
+      <div className="btn-container">
+        <div className="btn" onClick={() => navigate('/')}>
+          <FontAwesomeIcon icon={faBicycle} fixedWidth />
+          找單車
+        </div>
+        <div className="btn" onClick={() => navigate('/bike-routes')}>
+          <FontAwesomeIcon icon={faRoute} fixedWidth />
+          找路線
+        </div>
+        <div className="btn" onClick={() => navigate('/scenic-spots')}>
+          <FontAwesomeIcon icon={faUmbrellaBeach} fixedWidth />
+          找景點
+        </div>
+      </div>
     </header>
   );
 };
